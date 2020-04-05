83da1706ff234c3ac647e04e2d5bdeb10376b11e
matcs <55284492+matcs@users.noreply.github.com>
2020-04-05 18:05:02 -0300

    Initial commit

---
+++ backend/src/test/java/com/pizzaria/Controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzaria.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
