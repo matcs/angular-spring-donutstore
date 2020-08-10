076b18bed862e86e24a03ff1cc2aec36f652b045
matcs <55284492+matcs@users.noreply.github.com>
2020-08-10 16:15:45 -0300

    pasta donut deletada

--- donut/src/main/java/com/donutstore/Repository/ClienteRepositoy.java
+++
@@ -1,9 +0,0 @@
-package com.donutstore.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.donutstore.Model.Cliente;
-
-public interface ClienteRepositoy extends JpaRepository<Cliente, Long> {
-	Cliente findByIdCliente(long idCliente);
-}
--- donut/src/main/java/com/donutstore/Repository/DonutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.donutstore.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.donutstore.Model.Donut;
-
-public interface DonutRepository extends JpaRepository<Donut, Long> {
-	Donut findByIdDonut(long idDonut);
-}
